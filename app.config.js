95f53e1ad28a315c825787050f288accedf42315
LuanRigatti <luan.rigatti@hotmail.com>
2026-08-13 16:22:52 -0300

    chore: rename iOS app display name to Rigatti

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ const androidMapsKey = process.env.EXPO_PUBLIC_GOOGLE_MAPS_ANDROID_KEY;
 
 module.exports = {
   expo: {
-    name: 'PAReact',
+    name: 'Rigatti',
     slug: 'PAReact',
     version: '1.0.0',
     orientation: 'portrait',
